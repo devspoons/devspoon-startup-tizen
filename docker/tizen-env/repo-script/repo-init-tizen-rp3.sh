55ae69f1b1b127d4844290d4b1271f4b52d91ed1
bluebamus <bluebamus@naver.com>
2020-04-17 10:21:14 -0700

    add new sh file for tizen rp3 env

---
+++ docker/tizen-env/repo-script/repo-init-tizen-rp3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+repo init -u tizen:scm/manifest -b tizen -m unified_standard.xml
+
+wget http://download.tizen.org/releases/weekly/tizen/unified/tizen-unified_20190807.5/builddata/manifest/tizen-unified_20190807.5_standard_preloadapp.xml -O .repo/manifests/unified/standard/projects.xml
+
+sed -i '3,4d' .repo/manifests/unified/standard/projects.xml
+
+repo sync -j 16
+
+cp /root/tizen_project/repo-script/.gbs.conf ./
