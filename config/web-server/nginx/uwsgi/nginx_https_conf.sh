b3a4b31e115fd3e24f0157c1c989f365bbf76d88
devspoon <bluebamus@naver.com>
2020-06-12 01:00:01 +0900

    bug fix nginx web server config script

--- config/web-server/nginx/uwsgi/nginx_https_conf.sh
+++ config/web-server/nginx/uwsgi/nginx_https_conf.sh
@@ -9,7 +9,7 @@ service_port=$5
 sed 's/account/'$account'/' sample_nginx_https.conf > $account'1'.temp
 sed 's/domain/'$domain'/g' $account'1'.temp > $account'2'.temp
 sed 's/portnumber;/'$portnumber';/' $account'2'.temp > $account'3'.temp
-sed 's/appname;/'$appname';/' $account'3'.temp > $account'4'.temp
+sed 's/appname/'$appname'/' $account'3'.temp > $account'4'.temp
 sed 's/service_port/'$service_port'/' $account'4'.temp > ./sites-available/$account'_https_ng'.conf 
 
 rm *.temp
