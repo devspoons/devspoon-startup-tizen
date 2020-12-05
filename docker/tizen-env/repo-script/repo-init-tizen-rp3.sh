31e073a2e43868bccf2d55474d83b49c96bb4653
devspoon <bluebamus@naver.com>
2020-12-05 23:35:55 +0900

    added new job in jenkins about repo init for rpi3 case.

--- docker/tizen-env/repo-script/repo-init-tizen-rp3.sh
+++ docker/tizen-env/repo-script/repo-init-tizen-rp3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir ~/tizen_src && cd ~/tizen_src
+
 repo init -u tizen:scm/manifest -b tizen -m unified_standard.xml
 
 wget http://download.tizen.org/releases/weekly/tizen/unified/tizen-unified_20190807.5/builddata/manifest/tizen-unified_20190807.5_standard_preloadapp.xml -O .repo/manifests/unified/standard/projects.xml
@@ -8,4 +10,4 @@ sed -i '3,4d' .repo/manifests/unified/standard/projects.xml
 
 repo sync -j 16
 
-cp /root/tizen_project/repo-script/.gbs.conf ./
+cp /root/tizen_project/repo-script/.gbs.conf /root/tizen_src
