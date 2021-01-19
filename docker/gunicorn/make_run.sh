5d1b64db9d6dee96f73104034b7e525ed4afc6f2
bluebamus <bluebamus@naver.com>
2021-01-20 00:20:54 +0900

    The run.sh file has been updated to set up creating log files and checking frameworks between django and flask in gunicorn's docker.

--- docker/gunicorn/make_run.sh
+++ docker/gunicorn/make_run.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+django=1
+flask=2
+
 while :
 do 
-    echo "* if your project path has sub-level, you should be insert as \\\/A\\\/B\\\/C"
+    echo "Select Framework Type 1. django, 2.flask"
+    echo -n "Enter the Type >"
+    read fwtype
+    echo  "Entered the Type: $fwtype"
+    if [[ "$fwtype" != "" ]]; then
+        break
+    fi
+done 
+
+while :
+do 
+    echo "* if your project path has sub-level, you should be insert as \\\/www\\\/B\\\/C"
     echo -n "Enter the project path >"
     read project_path
     echo  "Entered the project path: $project_path"
@@ -21,7 +35,18 @@ do
     fi
 done 
 
+if [[ "$fwtype" -eq $django ]]; then
+    wsgi=${project_name}".wsgi:application"
+    echo  "django wsgi: $wsgi"
+else
+    echo -n "Enter a flask wsgi app name >"
+    read appname
+    wsgi="wsgi:"${appname}
+    echo  "flask wsgi: $wsgi"
+fi
+
 sed 's/project_path/'$project_path'/g' sample_run > $project_name'1'.temp
-sed 's/project_name/'$project_name'/g' $project_name'1'.temp > run.sh
+sed 's/wsgi/'$wsgi'/g' $project_name'1'.temp > $project_name'2'.temp
+sed 's/project_name/'$project_name'/g' $project_name'2'.temp > run.sh
 
 rm *.temp
