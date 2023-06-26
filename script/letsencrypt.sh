452d3e9c57e60cffd90eeddaf4e6012dad7db9ec
bluebamus <bluebamus@naver.com>
2023-06-26 22:00:37 +0900

    update letsencrypt.sh and config file of nginx

--- script/letsencrypt.sh
+++ script/letsencrypt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+my_array=()
+delimiter="-d"
+domain_string=""
+
 apt-get update  &&  apt-get install -y sendmail wget vim cron certbot python3-certbot-nginx ca-certificates
 while :
 do 
@@ -13,7 +17,7 @@ done
 
 while :
 do 
-    echo -n "If you want to add subdomain, insert it after '-d' option 'aaa.com -d www.aaa.com -d sub.aaa.com'"
+    echo -n "To add a subdomain, type something like 'aaa.com www.aaa.com sub.aaa.com'."
     echo -n "Enter the service domain >"
     read domain
     echo  "Entered service domain: $domain"
@@ -21,6 +25,8 @@ do
         break
     fi
 done 
+
+IFS=' ' read -ra my_array <<< "$domain"
 
 while :
 do 
@@ -32,29 +38,38 @@ do
     fi
 done 
 
-#if ! test -f /etc/ssl/certs/dhparam.pem ; 
-if ! test -f /etc/ssl/certs/$domain/dhparam.pem ; then 
-    echo "try to create ssl key using openssl "
-    if ! test -d /etc/ssl/certs/ ; then
-        echo "create domain folder: /etc/ssl/certs/"
-        mkdir -p /etc/ssl/certs/
+for element in "${my_array[@]}"; do
+    domain_string+=" $delimiter $element"
+done
+
+# Remove leading space
+# domain_string="${domain_string# }"
+
+for element in "${my_array[@]}"; do
+    #if ! test -f /etc/ssl/certs/dhparam.pem ; 
+    if ! test -f /etc/ssl/certs/$element/dhparam.pem ; then 
+        echo "try to create ssl key using openssl "
+        if ! test -d /etc/ssl/certs/ ; then
+            echo "create domain folder: /etc/ssl/certs/"
+            mkdir -p /etc/ssl/certs/
+        fi
+        openssl dhparam -out /etc/ssl/certs/$element/dhparam.pem 4096
+        # if ! test -d /ssl/certs/$domain/ ; then
+        #     echo "create domain folder: /ssl/certs/"$domain"/"
+        #     mkdir -p /ssl/certs/$domain/
+        # fi
+        # cp /etc/ssl/certs/dhparam.pem /ssl/certs/$domain/ -r
+    # else
+    #     echo "copy ssl folder by already maden"
+    #     cp /ssl/certs/$domain/dhparam.pem /etc/ssl/certs/dhparam.pem -r
     fi
-    openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
-    # if ! test -d /ssl/certs/$domain/ ; then
-    #     echo "create domain folder: /ssl/certs/"$domain"/"
-    #     mkdir -p /ssl/certs/$domain/
-    # fi
-    # cp /etc/ssl/certs/dhparam.pem /ssl/certs/$domain/ -r
-# else
-#     echo "copy ssl folder by already maden"
-#     cp /ssl/certs/$domain/dhparam.pem /etc/ssl/certs/dhparam.pem -r
-fi
+done
 
 #if ! test -d /etc/letsencrypt/live/test.com ; 
 if ! test -d /etc/ssl/letsencrypt/$domain/letsencrypt ; then 
     echo "try to create authentication key using certbot "
-    certbot certonly --agree-tos --email $mail --webroot -w $webroot_folder -d $domain
-    echo "certbot certonly --agree-tos --email "$mail" --webroot -w "$webroot_folder" -d "$domain
+    certbot certonly --agree-tos --email $mail --webroot -w $webroot_folder $domain
+    echo "certbot certonly --agree-tos --email "$mail" --webroot -w "$webroot_folder" -d "$domain_string
     # if ! test -d /ssl/letsencrypt/$domain/ ; then
     #     echo "create domain folder: /ssl/letsencrypt/"$domain"/"
     #     mkdir -p /ssl/letsencrypt/$domain/
@@ -66,3 +81,5 @@ if ! test -d /etc/ssl/letsencrypt/$domain/letsencrypt ; then
 fi
 
 # cat <(crontab -l) <(echo "0 5 * * 1 certbot renew --quiet --renew-hook \"service nginx reload\"") | crontab -
+
+done 
