8a87e174b8a01f9baebc80f9f0ba3290ca5b1f94
bluebamus <bluebamus@naver.com>
2023-06-30 17:33:56 +0900

    docker-compose.yml update for using config

--- config/app-server/gunicorn/gunicorn_config.py
+++ config/app-server/gunicorn/gunicorn_config.py
@@ -1,4 +1,6 @@
 import multiprocessing
 
-bind = "127.0.0.1:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+bind = "0.0.0.0:8000"
+#workers = multiprocessing.cpu_count() * 2 + 1
+workers = 4
+wsgi_app = 'django_sample.wsgi:application'
