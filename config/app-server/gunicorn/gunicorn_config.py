893d585f6c35ea9f4e48727782c5cf8ce1d8e919
bluebamus <bluebamus@naver.com>
2023-11-05 22:48:02 +0900

    update config/app-server/gunicorn/gunicorn_config.py

--- config/app-server/gunicorn/gunicorn_config.py
+++ config/app-server/gunicorn/gunicorn_config.py
@@ -1,6 +1,12 @@
 import multiprocessing
+from datetime import datetime
+import os
 
 bind = "0.0.0.0:8000"
 # workers = multiprocessing.cpu_count() * 2 + 1
 workers = 4
-wsgi_app = "django_sample.wsgi:application"
+wsgi_app = "config.wsgi:application"
+reload = True
+accesslog = f"/log/gunicorn/access_{datetime.now().strftime('%Y-%m-%d_%H')}.log"
+errorlog = f"/log/gunicorn/error_{datetime.now().strftime('%Y-%m-%d_%H')}.log"
+loglevel = "info"
